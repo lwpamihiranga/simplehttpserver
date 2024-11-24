e36bbd05e0f1b01533644d11b05833bf242b555b
Amith Mihiranga <lwpamihiranga@gmail.com>
2024-11-24 11:55:06 +0530

    Initialize http driver class

---
+++ src/main/java/com/amithmihiranga/httpserver/HttpServer.java
@@ -0,0 +1,12 @@
+package com.amithmihiranga.httpserver;
+
+/**
+ *
+ * Driver class for HTTP Server
+ *
+ */
+public class HttpServer {
+    public static void main(String[] args) {
+        System.out.println("Server starting...");
+    }
+}
