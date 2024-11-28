6fb19825b24c934dc3d2155c976fb3eacfdd1954
Amith Mihiranga <lwpamihiranga@gmail.com>
2024-11-28 13:21:59 +0530

    Write simple html output

--- src/main/java/com/amithmihiranga/httpserver/HttpServer.java
+++ src/main/java/com/amithmihiranga/httpserver/HttpServer.java
@@ -1,5 +1,11 @@
 package com.amithmihiranga.httpserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * Driver class for HTTP Server
@@ -8,5 +14,32 @@ package com.amithmihiranga.httpserver;
 public class HttpServer {
     public static void main(String[] args) {
         System.out.println("Server starting...");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(8080);
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            String html = "<html><p>Hello, world!</p></html>";
+
+            final String CRLF = "\n\r";
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF +
+                    "Content-Length: " + html.getBytes().length + CRLF +
+                    CRLF +
+                    html +
+                    CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
